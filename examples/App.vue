ac3abc80c138ab7d00876394441c5d5de8f6f1f2
Nino Vrijman <ninovrijman@gmail.com>
2019-03-13 11:52:36 +0100

    Set-up project, example and basic build scripts

---
+++ examples/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/wrapper.js
@@ -0,0 +1,33 @@
+// Import vue component
+// import component from './my-component.vue';
+
+const components = {
+  // VueSplitGrid
+  // VueSplitGridPart?
+}
+
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('VueSplitGrid', component);
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// To allow use as module (npm/webpack/etc.) export component
+export default components;
